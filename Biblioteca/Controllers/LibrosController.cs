2652397cb14e72b608589f1b09e040d42c8bb5b6
Facundo Barriola <facubarriola7@gmail.com>
2023-07-14 15:32:37 -0300

    Se agrego controlador para libros y se crearon carpetas repositories y service

--- Biblioteca/Controllers/LibrosController.cs
+++ Biblioteca/Controllers/LibrosController.cs
@@ -1,6 +1,16 @@
-﻿namespace Biblioteca.Controllers
+﻿using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
 {
+    [ApiController]
+    [Route("api/libros")]
     public class LibrosController
     {
+        //[HttpGet]
+        //public async Task<ActionResult<List<Libro>>> Get() 
+        //{
+            
+        //}
     }
 }
