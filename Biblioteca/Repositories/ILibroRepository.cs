00adf52dffeec5628c6bcdb913212aef8eabf52d
Facundo Barriola <facubarriola7@gmail.com>
2023-07-14 16:09:20 -0300

    Se crearon los métodos para crear, elminiar y editar un libro

--- Biblioteca/Repositories/ILibroRepository.cs
+++ Biblioteca/Repositories/ILibroRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Biblioteca.Repositories
+﻿using Biblioteca.Models;
+
+namespace Biblioteca.Repositories
 {
     public interface ILibroRepository
     {
+        IEnumerable<Libro> GetAll();
+        void Create(Libro libro);
+        void Update(Libro libro);
+        void Delete(int id);
+        IEnumerable<Libro> GetLibroByEstante(int id_estante);
     }
 }
--- Biblioteca/Repositories/LibroRepository.cs
+++ Biblioteca/Repositories/LibroRepository.cs
@@ -1,6 +1,32 @@
-﻿namespace Biblioteca.Repositories
+﻿using Biblioteca.Models;
+
+namespace Biblioteca.Repositories
 {
-    public class LibroRepository
+    public class LibroRepository : ILibroRepository
     {
+        public void Create(Libro libro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Libro> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Libro> GetLibroByEstante(int id_estante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Libro libro)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
