842c6a41dd8454a2939710fab621b8664c41765b
Facundo Barriola <facubarriola7@gmail.com>
2023-07-14 14:27:50 -0300

    Se agrego carpeta conexion junto con su respectiva clase para poder conectar con la base de datos

---
+++ Biblioteca/Conexion/ConexionDB.cs
@@ -0,0 +1,20 @@
+﻿namespace Biblioteca.Conexion
+{
+    public class ConexionDB
+    {
+        private string connectionString = string.Empty;
+        public ConexionDB() 
+        {
+            // Construimos la conexión con la base de datos
+            var constructor = new ConfigurationBuilder().SetBasePath
+                    (Directory.GetCurrentDirectory()).AddJsonFile
+                    ("appsettings.json").Build();
+            connectionString = constructor.GetSection("ConnectionStrings:conexionmaestra").Value;
+        }
+
+        public string cadenaSQL() 
+        {
+            return connectionString;
+        }
+    }
+}
