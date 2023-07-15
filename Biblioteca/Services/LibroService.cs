5fe2f82fb4fbb32ecaa39e2f89cc768d4d7865c5
Facundo Barriola <facubarriola7@gmail.com>
2023-07-15 15:30:32 -0300

    Se agregaron métodos crud

--- Biblioteca/Services/LibroService.cs
+++ Biblioteca/Services/LibroService.cs
@@ -1,12 +1,24 @@
 ﻿using Biblioteca.Models;
+using Biblioteca.Repositories;
 
 namespace Biblioteca.Services
 {
     public class LibroService : ILibroService
     {
+        private LibroRepository _libroRespository;
         public IEnumerable<Libro> GetAllLibros()
         {
-            throw new NotImplementedException();
+            return _libroRespository.GetAll();
+        }
+
+        public void CreateLibro(Libro libro) 
+        {
+            _libroRespository.Create(libro);
+        }
+
+        public void DeleteLibro(int idLibro) 
+        {
+            _libroRespository.Delete(idLibro);
         }
     }
 }
