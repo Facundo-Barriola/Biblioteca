8ddbd901059257af48a6ff836da49ffb83f3c12f
Facundo Barriola <facubarriola7@gmail.com>
2023-07-15 15:29:16 -0300

    Se agregaron métodos CRUD

--- Biblioteca/Services/ILibroService.cs
+++ Biblioteca/Services/ILibroService.cs
@@ -5,5 +5,8 @@ namespace Biblioteca.Services
     public interface ILibroService
     {
         IEnumerable<Libro> GetAllLibros();
+
+        void CreateLibro(Libro libro);
+        void DeleteLibro(int idLibro);
     }
 }
