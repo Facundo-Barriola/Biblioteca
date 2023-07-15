86f9762e1fc188d3409d2c9543c6225df6118026
SantiagoMonzalvo <santimonzalvo@hotmail.com>
2023-07-15 15:28:59 -0300

    Se agregó EstanteRepository y IEstanteRepository, con metodos CRUD y gets de LINQ

---
+++ Biblioteca/Repositories/EstanteRepositories.cs
@@ -0,0 +1,62 @@
+﻿using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Biblioteca.Repositories
+{
+    public class EstanteRepository : IEstanteRepository
+    {
+        private readonly BibliotecaContext _context;
+
+        public EstanteRepository(BibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        /// GetById y GetAll con sintáxis similar a SQL implementado con LINQ
+        /// SaveChanges es un método que no se declara en BibliotecaContext porque ya viene incluído con Entity Framework Core
+
+        ///Incluir a "Seccions" permite que al obtener un estante, también se lean las secciones asociadas
+        public Estante GetById(int id)
+        {
+            return _context.Estantes
+                .Include(e => e.Seccions)
+                .FirstOrDefault(e => e.IdEstante == id);
+        }
+
+        public IEnumerable<Estante> GetAll()
+        {
+            return _context.Estantes
+                .Include(e => e.Seccions)
+                .ToList();
+        }
+
+        public void Add(Estante estante)
+        {
+            _context.Estantes.Add(estante);
+            _context.SaveChanges();
+        }
+
+        public void Update(Estante estante)
+        {
+            _context.Estantes.Update(estante);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Estante estante)
+        {
+            _context.Estantes.Remove(estante);
+            _context.SaveChanges();
+        }
+    }
+
+    public interface IEstanteRepository
+    {
+        Estante GetById(int id);
+        IEnumerable<Estante> GetAll();
+        void Add(Estante estante);
+        void Update(Estante estante);
+        void Delete(Estante estante);
+    }
+}
