b5bca2f465165b3fabe9200840cddac3eb32aeed
SantiagoMonzalvo <santimonzalvo@hotmail.com>
2023-07-18 12:30:03 -0300

    Creación de Controlador para Seccion

---
+++ Biblioteca/Controllers/SeccionController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Biblioteca.Models;
+using Biblioteca.Services;
+
+namespace Biblioteca.Controllers
+{
+    [ApiController]
+    [Route("api/secciones")]
+    public class SeccionController : ControllerBase
+    {
+        private readonly SeccionService _seccionService;
+
+        public SeccionController(SeccionService seccionService)
+        {
+            _seccionService = seccionService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Seccion>> GetAllSecciones()
+        {
+            var secciones = _seccionService.GetAllSecciones();
+            return Ok(secciones);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Seccion> GetSeccionById(int id)
+        {
+            var seccion = _seccionService.GetSeccionById(id);
+            if (seccion == null)
+            {
+                return NotFound();
+            }
+            return Ok(seccion);
+        }
+
+        [HttpPost]
+        public IActionResult AddSeccion([FromBody] Seccion seccion)
+        {
+            _seccionService.AddSeccion(seccion.IdSeccion, seccion.DescripcionSeccion, seccion.IdEstante);
+            return CreatedAtAction(nameof(GetSeccionById), new { id = seccion.IdSeccion }, seccion);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateSeccion(int id, [FromBody] Seccion seccion)
+        {
+            if (id != seccion.IdSeccion)
+            {
+                return BadRequest();
+            }
+
+            _seccionService.UpdateSeccion(seccion.IdSeccion, seccion.DescripcionSeccion, seccion.IdEstante);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteSeccion(int id)
+        {
+            _seccionService.DeleteSeccion(id);
+            return NoContent();
+        }
+    }
+}
