e32472ed799b8d24c5b7f1aea5ecca6e429724e3
Facundo Barriola <facubarriola7@gmail.com>
2023-07-19 13:46:59 -0300

    Se quitaron métodos

--- BibliotecaDB/Libro.cs
+++ BibliotecaDB/Libro.cs
@@ -8,7 +8,7 @@ namespace BibliotecaDB
 { 
     public class Libro
     {
-     [Key]
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdLibro { get; set; }
 
@@ -27,45 +27,8 @@ namespace BibliotecaDB
         public Seccion IdSeccionNavigation { get; set; } = null!;
         public ICollection<PrestamoLibro> PrestamoLibros { get; set; } = new List<PrestamoLibro>();
 
-        public Libro(int idLibro, string titulo, string sinopsis, int puntajeCritica,
-            int estado, bool disponibilidad, int idSeccion) 
-        {
-            IdLibro = idLibro;
-            Titulo = titulo; 
-            Sinopsis = sinopsis;
-            PuntajeCritica = puntajeCritica;
-            Estado = estado;
-            Disponibilidad = disponibilidad;
-            IdSeccion = idSeccion;
-        }
-
-        public void UpdateTitulo(string newTitulo) 
-        {
-            Titulo = newTitulo;
-        }
-        public void UpdateSinopsis(string newSinopsis) 
-        {
-            Sinopsis = newSinopsis;
-        }
-        public void UpdatePuntaje(int newPuntaje) 
-        {
-            PuntajeCritica = newPuntaje;
-        }
-        public void UpdateEstado(int newEstado) 
-        {
-            Estado = newEstado;
-        }
-        //Consultar si es mejor realizarlo con int o con string
-        public void UpdateDisponibilidad(int newDisponibilidad) 
-        {
-            if (newDisponibilidad == 1)
-            {
-                Disponibilidad = true;
-            }
-            else { Disponibilidad = false; } 
-
         }
     }
 
-}
+
 
