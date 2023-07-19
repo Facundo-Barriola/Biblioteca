8647412bd0c7fc6ad7ecb9f2d88b8eedac78d2a6
SantiagoMonzalvo <santimonzalvo@hotmail.com>
2023-07-19 14:38:23 -0300

    Se creó el Servicio para Prestamo

---
+++ Biblioteca/Service/PrestamoService.cs
@@ -0,0 +1,47 @@
+﻿using Biblioteca.Models;
+using Biblioteca.Repositories;
+using System.Collections.Generic;
+
+namespace Biblioteca.Services
+{
+    public class PrestamoService
+    {
+        private IPrestamoRepository _prestamoRepository;
+
+        public PrestamoService(IPrestamoRepository prestamoRepository)
+        {
+            _prestamoRepository = prestamoRepository;
+        }
+
+        public List<Prestamo> GetAllPrestamos()
+        {
+            return _prestamoRepository.GetAll();
+        }
+
+        public Prestamo GetPrestamoById(int id)
+        {
+            return _prestamoRepository.GetById(id);
+        }
+
+        public void AddPrestamo(int idPrestamo, DateTime? fechaExtraccion, DateTime fechaDevolucion, DateTime fechaPactada, bool? estadoPrestamo, string idUsuario)
+        {
+            Prestamo prestamo = new Prestamo(idPrestamo, fechaExtraccion, fechaDevolucion, fechaPactada, estadoPrestamo, idUsuario);
+            _prestamoRepository.Add(prestamo);
+        }
+
+        public void UpdatePrestamo(int idPrestamo, DateTime? fechaExtraccion, DateTime fechaDevolucion, DateTime fechaPactada, bool? estadoPrestamo, string idUsuario)
+        {
+            Prestamo prestamo = new Prestamo(idPrestamo, fechaExtraccion, fechaDevolucion, fechaPactada, estadoPrestamo, idUsuario);
+            _prestamoRepository.Update(prestamo);
+        }
+
+        public void DeletePrestamo(int id)
+        {
+            var prestamo = _prestamoRepository.GetById(id);
+            if (prestamo != null)
+            {
+                _prestamoRepository.Delete(prestamo);
+            }
+        }
+    }
+}
