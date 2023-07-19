0dea96e60b1eeadf855c7914439274f441cdd88a
SantiagoMonzalvo <santimonzalvo@hotmail.com>
2023-07-19 15:42:20 -0300

    Se agregó Servicio para PrestamoLibro

---
+++ Biblioteca/Service/PrestamoLibroService.cs
@@ -0,0 +1,47 @@
+﻿using Biblioteca.Models;
+using Biblioteca.Repositories;
+using System.Collections.Generic;
+
+namespace Biblioteca.Services
+{
+    public class PrestamoLibroService
+    {
+        private IPrestamoLibroRepository _prestamoLibroRepository;
+
+        public PrestamoLibroService(IPrestamoLibroRepository prestamoLibroRepository)
+        {
+            _prestamoLibroRepository = prestamoLibroRepository;
+        }
+
+        public List<PrestamoLibro> GetAllPrestamosLibros()
+        {
+            return _prestamoLibroRepository.GetAll();
+        }
+
+        public PrestamoLibro GetPrestamoLibroById(int id)
+        {
+            return _prestamoLibroRepository.GetById(id);
+        }
+
+        public void AddPrestamoLibro(int idPrestamoLibro, int idPrestamo, int idLibro)
+        {
+            PrestamoLibro prestamoLibro = new PrestamoLibro(idPrestamoLibro, idPrestamo, idLibro);
+            _prestamoLibroRepository.Add(prestamoLibro);
+        }
+
+        public void UpdatePrestamoLibro(int idPrestamoLibro, int idPrestamo, int idLibro)
+        {
+            PrestamoLibro prestamoLibro = new PrestamoLibro(idPrestamoLibro, idPrestamo, idLibro);
+            _prestamoLibroRepository.Update(prestamoLibro);
+        }
+
+        public void DeletePrestamoLibro(int id)
+        {
+            var prestamoLibro = _prestamoLibroRepository.GetById(id);
+            if (prestamoLibro != null)
+            {
+                _prestamoLibroRepository.Delete(prestamoLibro);
+            }
+        }
+    }
+}
