8d2d0abc4cb3cbe3f521652c89a3c32f95392a8f
Facundo Barriola <facubarriola7@gmail.com>
2023-07-18 23:15:39 -0300

    Se agrego método para las migraciones de la DB

--- Biblioteca/Program.cs
+++ Biblioteca/Program.cs
@@ -20,6 +20,12 @@ builder.Services.AddScoped<LibroService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope()) 
+{
+    var context = scope.ServiceProvider.GetRequiredService<BibliotecaContext>();
+    context.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
