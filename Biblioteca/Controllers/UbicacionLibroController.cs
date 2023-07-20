847f97f7e0474b6e27cbfe7f2da90fd4c0017e1d
SantiagoMonzalvo <santimonzalvo@hotmail.com>
2023-07-20 17:44:13 -0300

    Creación de Controlador para obtener Ubicación de un Libro, accediendo a los repositorios de las entidades: Libro, Seccion, Estante, Estanteria y Salon

---
+++ Biblioteca/Controllers/UbicacionLibroController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Biblioteca.Models;
+using Biblioteca.Services;
+
+namespace Biblioteca.Controllers
+{
+    [ApiController]
+    [Route("api/ubicaciones")]
+    public class UbicacionController : ControllerBase
+    {
+        private readonly IUbicacionService _ubicacionService;
+
+        public UbicacionController(IUbicacionService ubicacionService)
+        {
+            _ubicacionService = ubicacionService;
+        }
+
+        [HttpGet("{idLibro}")]
+        public IActionResult ObtenerUbicacionLibro(int idLibro)
+        {
+            try
+            {
+                string ubicacion = _ubicacionService.ObtenerUbicacionLibro(idLibro);
+                return Ok(ubicacion);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
