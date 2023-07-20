1b7a6b46e5269b4fe9d7277ee0be446838cb53ab
Facundo Barriola <facubarriola7@gmail.com>
2023-07-20 20:46:40 -0300

    Creación de Servicio de usuarios y implementación de métodos CRUD

--- Biblioteca/Services/UsuarioService.cs
+++ Biblioteca/Services/UsuarioService.cs
@@ -1,7 +1,39 @@
-﻿namespace Biblioteca.Services
+﻿using Biblioteca.Models;
+using Biblioteca.Repositories;
+
+namespace Biblioteca.Services
 {
     public class UsuarioService
     {
+        private UsuarioRepository _usuarioRepository;
+        public UsuarioService(UsuarioRepository usuarioRepository) 
+        {
+            _usuarioRepository = usuarioRepository;
+        }
 
+        public List<Usuario> TraerTodosUsuarios() 
+        {
+            return _usuarioRepository.TraerTodos();
+        }
+
+        public Usuario InsertarUsuario(Usuario usuario) 
+        {
+            return _usuarioRepository.Insertar(usuario);
+        }
+
+        public void BorrarUsuario(int idUsuario) 
+        {
+            _usuarioRepository.Borrar(idUsuario);
+        }
+
+        public Usuario EditarUsuario(Usuario usuario) 
+        {
+            return _usuarioRepository.Editar(usuario); 
+        }
+
+        public Usuario BuscarUsuarioPorId(int id) 
+        {
+            return _usuarioRepository.BuscarPorId(id);
+        }
     }
 }
