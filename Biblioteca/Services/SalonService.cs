f19cc685a4bd5093254d98ef415ec5183ec5eaa5
SantiagoMonzalvo <santimonzalvo@hotmail.com>
2023-07-20 22:29:11 -0300

    Se corrigió Servicio de Salon

--- Biblioteca/Services/SalonService.cs
+++ Biblioteca/Services/SalonService.cs
@@ -18,30 +18,30 @@ namespace Biblioteca.Services
             return _salonRepository.GetAll();
         }
 
-        public Salon GetSalonById(int id)
+        public Salon BuscarSalonPorId(int id)
         {
-            return _salonRepository.GetById(id);
+            return _salonRepository.BuscarPorId(id);
         }
 
-        public void AddSalon(int idSalon, string descripcionSalon)
+        public Salon InsertarSalon(int idSalon, string descripcionSalon)
         {
             Salon salon = new Salon(idSalon, descripcionSalon);
-            _salonRepository.Add(salon);
+            return _salonRepository.Insertar(salon);
         }
 
-        public void UpdateSalon(int idSalon, string descripcionSalon)
+        public void EditarSalon(Salon salon)
         {
-            Salon salon = new Salon(idSalon, descripcionSalon);
-            _salonRepository.Update(salon);
+            _salonRepository.Editar(salon);
         }
 
-        public void DeleteSalon(int id)
+        public void BorrarSalon(int id)
         {
-            var salon = _salonRepository.GetById(id);
+            var salon = _salonRepository.BuscarPorId(id);
             if (salon != null)
             {
-                _salonRepository.Delete(salon);
+                _salonRepository.Borrar(id);
             }
         }
+
     }
 }
