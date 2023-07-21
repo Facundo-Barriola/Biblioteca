943e148ee8e8b3b83b15ce4b3f904a25e40a8085
Facundo Barriola <facubarriola7@gmail.com>
2023-07-21 18:08:00 -0300

    Se agrego método para obtener la ubicación del libro

--- Biblioteca/Services/LibroService.cs
+++ Biblioteca/Services/LibroService.cs
@@ -1,11 +1,16 @@
 ﻿using Biblioteca.Models;
 using Biblioteca.Repositories;
+using System.Linq;
 
 namespace Biblioteca.Services
 {
     public class LibroService 
     {
         private ILibroRepository _libroRepository;
+        private ISeccionRepository _seccionRepository;
+        private IEstanteRepository _estanteRepository;
+        private IEstanteriaRepository _estanteriaRepository;
+        private ISalonRepository _salonRepository;
 
         public LibroService(ILibroRepository libroRepository) 
         {
@@ -36,6 +41,35 @@ namespace Biblioteca.Services
         public Libro BuscarLibroPorId(int idLibro) 
         {
             return _libroRepository.BuscarPorId(idLibro);
+        }
+
+        public string UbicacionLibro(Libro libro) 
+        {
+            var libroBuscado = _libroRepository.BuscarPorId(libro.IdLibro);
+
+            if(libroBuscado != null) 
+            {
+                var ubicacionLibro = (from libros in _libroRepository.GetAll()
+                                      join seccion in _seccionRepository.GetAll() on libro.IdSeccion equals seccion.IdSeccion
+                                      join estante in _estanteRepository.GetAll() on seccion.IdEstante equals estante.IdEstante
+                                      join estanteria in _estanteriaRepository.GetAll() on estante.IdEstanteria equals estanteria.IdEstanteria
+                                      join salon in _salonRepository.GetAll() on estanteria.IdSalon equals salon.IdSalon
+                                      where libros.IdLibro == libro.IdLibro
+                                      select new
+                                      {
+                                          Salon = salon.DescripcionSalon,
+                                          Estanteria = estanteria.DescripcionEstanteria,
+                                          Estante = estante.DescripcionEstante,
+                                          Seccion = seccion.DescripcionSeccion
+                                      }).FirstOrDefault();
+
+                if (libroBuscado != null) 
+                {
+                    return $"Ubicacion del libro: {ubicacionLibro.Salon}, {ubicacionLibro.Estanteria}, {ubicacionLibro.Estante}," +
+                        $" {ubicacionLibro.Seccion}";
+                }
+            }
+            return "Libro no enconrtado";
         }
     }
 }
