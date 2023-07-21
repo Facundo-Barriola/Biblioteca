17c00a9640397895fb5e7a1c1a953510f88d244a
SantiagoMonzalvo <santimonzalvo@hotmail.com>
2023-07-21 01:18:26 -0300

    Corrección de Controller de Sección

--- Biblioteca/Controllers/SeccionController.cs
+++ Biblioteca/Controllers/SeccionController.cs
@@ -9,55 +9,51 @@ namespace Biblioteca.Controllers
     [Route("api/secciones")]
     public class SeccionController : ControllerBase
     {
-        private readonly SeccionService _seccionService;
+        private SeccionService _seccionService;
 
         public SeccionController(SeccionService seccionService)
         {
             _seccionService = seccionService;
         }
 
+        /// GETALL
         [HttpGet]
-        public ActionResult<IEnumerable<Seccion>> GetAllSecciones()
+        public IActionResult GetAll()
         {
             var secciones = _seccionService.GetAllSecciones();
             return Ok(secciones);
         }
 
-        [HttpGet("{id}")]
-        public ActionResult<Seccion> GetSeccionById(int id)
+        /// ADD
+        [HttpPost]
+        public IActionResult Insertar([FromBody] Seccion seccion)
         {
-            var seccion = _seccionService.GetSeccionById(id);
-            if (seccion == null)
+            var nuevaSeccion = _seccionService.InsertarSeccion(seccion.IdSeccion, seccion.DescripcionSeccion, seccion.IdEstante);
+            return Ok(nuevaSeccion);
+        }
+
+        /// UPDATE
+        [HttpPut("{id}")]
+        public IActionResult Editar(int id, [FromBody] Seccion seccionActualizada)
+        {
+            var seccionActual = _seccionService.BuscarSeccionPorId(id);
+            if (seccionActual == null || seccionActualizada == null)
             {
                 return NotFound();
             }
-            return Ok(seccion);
-        }
+            seccionActual.DescripcionSeccion = seccionActualizada.DescripcionSeccion;
+            seccionActual.IdEstante = seccionActualizada.IdEstante;
 
-        [HttpPost]
-        public IActionResult AddSeccion([FromBody] Seccion seccion)
-        {
-            _seccionService.AddSeccion(seccion.IdSeccion, seccion.DescripcionSeccion, seccion.IdEstante);
-            return CreatedAtAction(nameof(GetSeccionById), new { id = seccion.IdSeccion }, seccion);
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult UpdateSeccion(int id, [FromBody] Seccion seccion)
-        {
-            if (id != seccion.IdSeccion)
-            {
-                return BadRequest();
-            }
-
-            _seccionService.UpdateSeccion(seccion.IdSeccion, seccion.DescripcionSeccion, seccion.IdEstante);
+            _seccionService.EditarSeccion(seccionActual);
             return NoContent();
         }
 
+        /// DELETE
         [HttpDelete("{id}")]
-        public IActionResult DeleteSeccion(int id)
+        public IActionResult Borrar(int id)
         {
-            _seccionService.DeleteSeccion(id);
-            return NoContent();
+            _seccionService.BorrarSeccion(id);
+            return Ok();
         }
     }
 }
