34f03dd860838a29c854c34c53e18f24c05aa257
SantiagoMonzalvo <santimonzalvo@hotmail.com>
2023-07-21 04:38:25 -0300

    Corrección Servicio UbicacionLibro

--- Biblioteca/Services/UbicacionLibroService.cs
+++ Biblioteca/Services/UbicacionLibroService.cs
@@ -6,11 +6,11 @@ namespace Biblioteca.Services
 {
     public class UbicacionService : IUbicacionService
     {
-        private readonly ILibroRepository _libroRepository;
-        private readonly ISeccionRepository _seccionRepository;
-        private readonly IEstanteRepository _estanteRepository;
-        private readonly IEstanteriaRepository _estanteriaRepository;
-        private readonly ISalonRepository _salonRepository;
+        private ILibroRepository _libroRepository;
+        private ISeccionRepository _seccionRepository;
+        private IEstanteRepository _estanteRepository;
+        private IEstanteriaRepository _estanteriaRepository;
+        private ISalonRepository _salonRepository;
 
         public UbicacionService(
             ILibroRepository libroRepository,
@@ -28,31 +28,31 @@ namespace Biblioteca.Services
 
         public string ObtenerUbicacionLibro(int idLibro)
         {
-            Libro libro = _libroRepository.GetById(idLibro);
+            Libro libro = _libroRepository.BuscarPorId(idLibro);
             if (libro == null)
             {
                 throw new Exception("El libro no existe");
             }
 
-            Seccion seccion = _seccionRepository.GetById(libro.IdSeccion);
+            Seccion seccion = _seccionRepository.BuscarPorId(libro.IdSeccion);
             if (seccion == null)
             {
                 throw new Exception("La sección no existe");
             }
 
-            Estante estante = _estanteRepository.GetById(seccion.IdEstante);
+            Estante estante = _estanteRepository.BuscarPorId(seccion.IdEstante);
             if (estante == null)
             {
                 throw new Exception("El estante no existe");
             }
 
-            Estanteria estanteria = _estanteriaRepository.GetById(estante.IdEstanteria);
+            Estanteria estanteria = _estanteriaRepository.BuscarPorId(estante.IdEstanteria);
             if (estanteria == null)
             {
                 throw new Exception("La estantería no existe");
             }
 
-            Salon salon = _salonRepository.GetById(estanteria.IdSalon);
+            Salon salon = _salonRepository.BuscarPorId(estanteria.IdSalon);
             if (salon == null)
             {
                 throw new Exception("El salón no existe");
