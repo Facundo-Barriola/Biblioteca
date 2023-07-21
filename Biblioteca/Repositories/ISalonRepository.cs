0a829c4c5dc23b08e182257cfcd12d371cb5c56c
SantiagoMonzalvo <santimonzalvo@hotmail.com>
2023-07-20 21:44:29 -0300

    Corrección de Interfaz de Repositorio Salon

--- Biblioteca/Repositories/ISalonRepository.cs
+++ Biblioteca/Repositories/ISalonRepository.cs
@@ -1,16 +1,13 @@
 ﻿using Biblioteca.Models;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Biblioteca.Repositories
 {
     public interface ISalonRepository
     {
-        Salon GetById(int id);
-        List<Salon> GetAll();
-        Salon Add(Salon salon);
-        Salon Update(Salon salon);
-        void Delete(Salon salon);
+        Salon<Salon> GetAll();
+        Salon Insertar(Salon salon);
+        void Editar(Salon salon);
+        void Borrar(int salonId);
+        Salon BuscarPorId(int idSalon);
     }
 }
