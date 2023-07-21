51b858739e3f72c806fd75febf8e90117f6e82ec
SantiagoMonzalvo <santimonzalvo@hotmail.com>
2023-07-21 00:55:56 -0300

    Corrección de Repositorio, Interfaz de Repositorio y Nombre Incorrecto de Archivo de Estante

---
+++ Biblioteca/Repositories/EstanteRepository.cs
@@ -0,0 +1,89 @@
+﻿using BibliotecaDB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.Linq;
+
+namespace Biblioteca.Repositories
+{
+    public class EstanteRepository : IEstanteRepository
+    {
+        private BibliotecaContext _context;
+
+        public EstanteRepository(BibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        public Models.Estante Insertar(Models.Estante estante)
+        {
+            var modelEstante = new Models.Estante
+            {
+                IdEstante = estante.IdEstante,
+                DescripcionEstante = estante.DescripcionEstante,
+                IdEstanteria = estante.IdEstanteria
+            };
+
+            _context.Estantes.Add(modelEstante);
+            _context.SaveChanges();
+            return estante;
+        }
+
+        public Models.Estante BuscarPorId(int id)
+        {
+            var dbEstante = _context.Estantes.FirstOrDefault(e => e.IdEstante == id);
+            if (dbEstante != null)
+            {
+                var estante = new Models.Estante
+                (
+                    dbEstante.IdEstante,
+                    dbEstante.DescripcionEstante,
+                    dbEstante.IdEstanteria
+                );
+                return estante;
+            }
+            return null;
+        }
+
+        public List<Models.Estante> GetAll()
+        {
+            var dbEstantes = _context.Estantes.ToList();
+            var modelEstantes = new List<Models.Estante>();
+            foreach (var dbEstante in dbEstantes)
+            {
+                modelEstantes.Add(new Models.Estante
+                {
+                    dbEstante.IdEstante,
+                    dbEstante.DescripcionEstante,
+                    dbEstante.IdEstanteria
+                });
+            }
+            return modelEstantes;
+        }
+
+        public void Editar(Models.Estante estante)
+        {
+            var dbEstante = _context.Estantes.FirstOrDefault(e => e.IdEstante == estante.IdEstante);
+            if (dbEstante != null)
+            {
+                dbEstante.DescripcionEstante = estante.DescripcionEstante;
+                dbEstante.IdEstanteria = estante.IdEstanteria;
+                _context.SaveChanges();
+            }
+        }
+
+        public void Borrar(int estanteId)
+        {
+            Estante estante = _context.Estantes.Find(estanteId);
+            if (estante != null)
+            {
+                _context.Estantes.Remove(estante);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("No se encontró la estante con el ID especificado");
+            }
+        }
+
+    }
+}
--- Biblioteca/Repositories/IEstanteRepository.cs
+++ Biblioteca/Repositories/IEstanteRepository.cs
@@ -1,16 +1,13 @@
 ﻿using Biblioteca.Models;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Biblioteca.Repositories
 {
     public interface IEstanteRepository
     {
-    Estante GetById(int id);
-    IEnumerable<Estante> GetAll();
-    void Add(Estante estante);
-    void Update(Estante estante);
-    void Delete(Estante estante);
+        Estante<Estante> GetAll();
+        Estante Insertar(Estante Estante);
+        void Editar(Estante Estante);
+        void Borrar(int estanteId);
+        Estante BuscarPorId(int estanteId);
     }
 }
