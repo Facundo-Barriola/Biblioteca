90d9fcf47af75d30678c82c2edafa758ad620692
Facundo Barriola <facubarriola7@gmail.com>
2023-07-24 21:45:06 -0300

    Se agregaron las propiedades para el modelo

--- VistasBiblioteca/Models/UbicacionLibro.cs
+++ VistasBiblioteca/Models/UbicacionLibro.cs
@@ -8,5 +8,10 @@ namespace VistasBiblioteca.Models
 {
     public class UbicacionLibro
     {
+        public string Titulo { get; set; }
+        public string DescripcionSalon { get; set; }
+        public string DescripcionEstanteria { get; set; }
+        public string DescripcionEstante { get; set; }
+        public string DescripcionSeccion { get; set; }
     }
 }
