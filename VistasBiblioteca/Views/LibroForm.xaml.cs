1624cab5089575c98ac0abb704886c2b1c26d4fd
Facundo-Barriola <fbarriola6@gmail.com>
2023-07-26 15:19:23 -0300

    Se agrego formulario para el libro

---
+++ VistasBiblioteca/Views/LibroForm.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VistasBiblioteca.Views
+{
+    /// <summary>
+    /// Lógica de interacción para LibroForm.xaml
+    /// </summary>
+    public partial class LibroForm : Page
+    {
+        public LibroForm()
+        {
+            InitializeComponent();
+            this.DataContext = new LibroForm();
+        }
+    }
+}
